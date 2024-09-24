Gradient inversion fix for a single client

artifact_scripts/gradientInversion/rog/attack_fedavg.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     manual_seed = 0
     torch.manual_seed(manual_seed)
     config_file = sys.argv[1] if len(sys.argv) > 1 else "config_fedavg.yaml"
-    clients = sys.argv[2] if len(sys.argv) > 2 else 1
+    clients = int(sys.argv[2]) if len(sys.argv) > 2 else 1
     main(config_file, manual_seed=manual_seed, clients=clients)

artifact_scripts/gradientInversion/rog/attack_vnodes.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
     seed = 0
     torch.manual_seed(seed)
     config_file = sys.argv[1] if len(sys.argv) > 1 else "config_fedavg.yaml"
-    clients = sys.argv[2] if len(sys.argv) > 2 else 1
+    clients = int(sys.argv[2]) if len(sys.argv) > 2 else 1
     chunks = int(sys.argv[3]) if len(sys.argv) > 3 else 1
     main(config_file, manual_seed=seed, client=clients, chunks=chunks)